776d3399828358acc4f35373d2afcdade687e9a1
PC1 <PC1@OGRETMEN>
2023-12-19 09:08:17 +0300

    18 Aralık son düzenleme

---
+++ IKYonetim/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKYonetim
+{
+    internal class Employee
+    {
+        string ad, soyad,tcNo;
+
+
+    }
+}
---
+++ IKYonetim/Program.cs
@@ -0,0 +1,111 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ArrayList kisiler = new ArrayList();
+        kisiler.Add("Ahmet");
+        kisiler.Add("Ali");
+        kisiler.Add("Selma");
+        kisiler.Add("Fatma");
+        kisiler.Add("Cem");
+        kisiler.Add("İsmail");
+        kisiler.Add("Celal");
+        kisiler.Add("Sidal");
+
+        char secim;
+        do
+        {
+            Console.WriteLine("Arama için......1");
+            Console.WriteLine("Silme için......2");
+            Console.WriteLine("Ekleme için.....3");
+            Console.WriteLine("Listeleme için..4");
+            Console.WriteLine("Çıkmak için.....X");
+            Console.Write("Tuşuna basın:");
+            secim = char.Parse(Console.ReadLine());
+            Console.Clear();
+            switch (secim)
+            {
+                case '1':
+                    Arama(kisiler);
+                    break;
+                case '2':
+                    Silme(kisiler);
+                    break;
+                case '3':
+                    Ekleme(kisiler);
+                    break;
+                case '4':
+                    Listele(kisiler);
+                    break;
+                default:
+                    Console.WriteLine("Yanlış Seçim");
+                    break;
+
+            }
+        } while (secim != 'X');
+
+    }
+
+    private static void Listele(ArrayList kisiler)
+    {
+       foreach(var isim in kisiler)
+        {
+            Console.WriteLine(isim);
+        }
+    }
+
+    private static void Ekleme(ArrayList kisiler)
+    {
+        Console.Write("Eklenecek isim:");
+        string isim = Console.ReadLine();
+
+        kisiler.Add(isim);
+    }
+
+    private static void Silme(ArrayList kisiler)
+    {
+
+        Console.Write("Silinecek isim:");
+        string isim = Console.ReadLine();
+        bool var = kisiler.Contains(isim);
+
+        if(var)
+        {
+            Console.Write("Silmek için...Y \n İptal için...N:");
+            char secim = char.Parse(Console.ReadLine());
+            if(secim=='Y')
+            {
+                kisiler.Remove(isim);
+                Console.WriteLine("Kişi silindi");
+            }
+            else
+            {
+                Console.WriteLine("Kişi Silinmedi");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Kişi Bulunamadı");
+        }
+
+       
+    }
+
+    private static void Arama(ArrayList kisiler)
+    {
+        Console.Write("Kisi ismi:");
+        string arananKisi = Console.ReadLine();
+
+        bool var=kisiler.Contains(arananKisi);
+
+        if (var)
+            Console.WriteLine("Kişi Ekli!!!");
+        else
+            Console.WriteLine("Kişi Ekli Degil!!!");
+    }
+}
